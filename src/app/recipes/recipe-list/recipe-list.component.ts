e02fb365ebf8a8def64912dd4d09de0e637828eb
Shuai <szheng3@uci.edu>
2019-08-08 15:28:40 -0500

    added universal

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
 import {Select} from '@ngxs/store';
 import {RecipesState} from 'src/app/store/recipes/recipes.state';
 import {Observable} from 'rxjs';
+import {Meta, Title} from '@angular/platform-browser';
+import {isPlatformBrowser} from '@angular/common';
 
 @Component({
   selector: 'app-recipe-list',
@@ -12,11 +14,23 @@ import {Observable} from 'rxjs';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  @Select(RecipesState.getRecipes)recipes$: Observable<Recipe[]>;
+  @Select(RecipesState.getRecipes) recipes$: Observable<Recipe[]>;
+  isBrowser: boolean;
 
-  constructor(private recipeService: RecipeService) {
+  constructor(private recipeService: RecipeService, private title: Title, private meta: Meta,@Inject(PLATFORM_ID) platformId) {
+    this.isBrowser = isPlatformBrowser(platformId);
   }
 
   ngOnInit() {
+    this.title.setTitle('First Component');
+    const metaItems = [
+      {name: 'description', content: 'First component description.'},
+      {name: 'twitter:title', content: 'Angular Universal Server Side Rendering | DevGlan'},
+      {name: 'twitter:description', content: 'In this article, we will be discussing about server-side rendering of Angular app.'},
+      {property: 'og:title', content: 'Angular Universal Server Side Rendering | DevGlan'},
+      {itemprop: 'name', content: 'First component name.'},
+      {itemprop: 'description', content: 'In this article, we will be discussing about server-side rendering of Angular app.'},
+    ];
+    this.meta.addTags(metaItems, true);
   }
 }
